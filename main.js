f47d25f9284b347c418ee86a23a28c551d08f009
KanwalHeer <160328670+KanwalHeer@users.noreply.github.com>
2024-03-22 01:07:23 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,38 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+async function main() {
+    const answer = await inquirer.prompt([
+        {
+            name: "firstNumber",
+            type: "number",
+            message: "Enter your first Number"
+        },
+        {
+            name: "secondNumber",
+            type: "number",
+            message: "Enter your second Number"
+        },
+        {
+            name: "operator",
+            type: "list",
+            message: "choose your operator",
+            choices: ["+", "-", "*", "/"]
+        }
+    ]);
+    if (answer.operator === "+") {
+        console.log(answer.firstNumber + answer.secondNumber);
+    }
+    else if (answer.operator === "-") {
+        console.log(answer.firstNumber - answer.secondNumber);
+    }
+    else if (answer.operator === "*") {
+        console.log(answer.firstNumber * answer.secondNumber);
+    }
+    else if (answer.operator === "/") {
+        console.log(answer.firstNumber / answer.secondNumber);
+    }
+    else {
+        console.log("Enter Valid Operator Please");
+    }
+}
+main();
